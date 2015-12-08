07078bf3cea1db2d399f5a7619339bc180b0ff6a
Laura Henze <laurahenze@gmail.com>
2015-12-08 17:02:12 -0500

    scrapping the current loading animation. adding flex box.

--- templates/components/component--thumbnails.tpl.php
+++ templates/components/component--thumbnails.tpl.php
@@ -1,4 +1,4 @@
-<div class="thumbnails-container views-u-1">
+  <div class="thumbnails-container views-u-1">
   <div id="book-navbar">
     <?php if (isset($pager)) : ?>
       <?php print $pager ?>
@@ -9,10 +9,8 @@
       <?php foreach ($items as $thumbnail) : ?>
         <div class="views-row" data-uri="<?php print $thumbnail['cm']['uri'] ?>" data-sequence="<?php print $thumbnail['sequence'] ?>" data-width="<?php print $thumbnail['cm']['width'] ?>" data-height="<?php print $thumbnail['cm']['height'] ?>" data-levels="<?php print $thumbnail['cm']['levels'] ?>" data-dwtLevels="<?php print $thumbnail['cm']['dwtLevels'] ?>" data-compositingLayerCount="<?php print $thumbnail['cm']['compositingLayerCount'] ?>">
 
-          <div class="loaderHolder"><img src="<?php print $GLOBALS['base_url'] . '/' . drupal_get_path('module', 'dlts_viewer');?>/images/loadhold.png" width="220" height="356"></div>
-          
 
-          <div class="thumbHolder">
+          <div class="thumbHolder ">
             <a href="<?php print $thumbnail['url'] ?>"><img src="<?php print $thumbnail['imageServeURI'] ?>" /></a>
           </div>
           <span class="page-number"><?php print $thumbnail['sequence'] ?></span>
