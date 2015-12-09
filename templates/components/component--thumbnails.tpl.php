a3deced25651accc98c5cad63697596393f19bf4
Laura Henze <laurahenze@gmail.com>
2015-12-09 13:44:47 -0500

    height is set for thumbnails using "scale" -- module code, SASS, templating

--- templates/components/component--thumbnails.tpl.php
+++ templates/components/component--thumbnails.tpl.php
@@ -1,4 +1,4 @@
-  <div class="thumbnails-container views-u-1">
+<div class="thumbnails-container views-u-1">
   <div id="book-navbar">
     <?php if (isset($pager)) : ?>
       <?php print $pager ?>
@@ -8,12 +8,14 @@
     <?php if (isset($items)) : ?>
       <?php foreach ($items as $thumbnail) : ?>
         <div class="views-row" data-uri="<?php print $thumbnail['cm']['uri'] ?>" data-sequence="<?php print $thumbnail['sequence'] ?>" data-width="<?php print $thumbnail['cm']['width'] ?>" data-height="<?php print $thumbnail['cm']['height'] ?>" data-levels="<?php print $thumbnail['cm']['levels'] ?>" data-dwtLevels="<?php print $thumbnail['cm']['dwtLevels'] ?>" data-compositingLayerCount="<?php print $thumbnail['cm']['compositingLayerCount'] ?>">
-
-
-          <div class="thumbHolder ">
-            <a href="<?php print $thumbnail['url'] ?>"><img src="<?php print $thumbnail['imageServeURI'] ?>" /></a>
+        
+          <div class="thumbHolder">
+            <a href="<?php print $thumbnail['url'] ?>">
+              <img class="thumbItem"  src="<?php print $thumbnail['imageServeURI'] ?>"  />   
+              <div class="loaderHolder"><img class="thumbItemloader"  src="<?php print $GLOBALS['base_url'] . '/' . drupal_get_path('module', 'dlts_viewer');?>/images/loadhold.png"  height="230" width="166"></div>
+              <span class="page-number"><?php print $thumbnail['sequence'] ?></span>
+            </a>
           </div>
-          <span class="page-number"><?php print $thumbnail['sequence'] ?></span>
         </div>
       <?php endforeach ?>
     <?php endif ?>
