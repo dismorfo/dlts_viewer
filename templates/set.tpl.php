4853d8185323572b16a17f5532101f5cc635b6d9
Ekaterina Pechekhonova <ekate@nyu.edu>
2017-07-30 14:18:00 -0400

    Added rows to set template

--- templates/set.tpl.php
+++ templates/set.tpl.php
@@ -5,15 +5,19 @@
     <?php endif; ?>
 </div>
 <div  class="views-g pane " >
-    <?php if (isset($items)) : ?>
-        <?php foreach ($items as $thumbnail) : ?>
-            <div class="<?php if ($thumbnail['sequence'] == $sequence) { print 'current-page '; } ?>views-row" data-uri="<?php print $thumbnail['cm']['uri'] ?>" data-sequence="<?php print $thumbnail['sequence'] ?>" data-width="<?php print $thumbnail['cm']['width'] ?>" data-height="<?php print $thumbnail['cm']['height'] ?>" data-levels="<?php print $thumbnail['cm']['levels'] ?>" data-dwtLevels="<?php print $thumbnail['cm']['dwtLevels'] ?>" data-compositingLayerCount="<?php print $thumbnail['cm']['compositingLayerCount'] ?>">
+    <?php if (isset($rows)) : ?>
+        <?php foreach ($rows as $row) : ?>
+    <div>
+            <?php foreach ($row as $thumbnail) : ?>
+              <div class="<?php if ($thumbnail['sequence'] == $sequence) { print 'current-page '; } ?>views-row" data-uri="<?php print $thumbnail['cm']['uri'] ?>" data-sequence="<?php print $thumbnail['sequence'] ?>" data-width="<?php print $thumbnail['cm']['width'] ?>" data-height="<?php print $thumbnail['cm']['height'] ?>" data-levels="<?php print $thumbnail['cm']['levels'] ?>" data-dwtLevels="<?php print $thumbnail['cm']['dwtLevels'] ?>" data-compositingLayerCount="<?php print $thumbnail['cm']['compositingLayerCount'] ?>">
                 <div class="thumbHolder">
                     <a href="<?php print $thumbnail['url'] ?>">
                         <img class="thumbItem"  src="<?php print $thumbnail['imageServeURI'] ?>"  />
                     </a>
                 </div>
-            </div>
+              </div>
+            <?php endforeach ?>
+    </div>
         <?php endforeach ?>
     <?php endif ?>
 </div>
