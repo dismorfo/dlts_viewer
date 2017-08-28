398bc067ca2687a1bfc0114160371c67dc3aed4c
Ekaterina Pechekhonova <ekate@nyu.edu>
2017-08-28 00:58:48 -0700

    Added type maps

--- templates/components/component--slider.tpl.php
+++ templates/components/component--slider.tpl.php
@@ -1,1 +1,3 @@
+<?php if ($sequence_count>1) : ?>
 <span id="<?php print $slider_id ?>"></span> <form><input id="slider_value" value="<?php print $sequence_number ?>"/></form> <span>/</span> <span class="sequence_count"><?php print $sequence_count ?></span>
+<?php endif; ?>
